e8c36fe9806fa40e1dbce61970c450248bf39be5
Markus Ferdinand Dablander <markus.dablander@gmx.at>
2024-05-15 19:09:20 +0200

    sicherung

--- sort_and_slice_ecfp_featuriser.py
+++ sort_and_slice_ecfp_featuriser.py
@@ -3,18 +3,18 @@ from rdkit.Chem import rdFingerprintGenerator
 
 
 
-def construct_sort_and_slice_ecfp_featuriser(mols_train, 
-                                             max_radius = 2, 
-                                             pharm_atom_invs = False, 
-                                             bond_invs = True, 
-                                             chirality = False, 
-                                             sub_counts = True, 
-                                             vec_dimension = 1024, 
-                                             break_ties_with = lambda sub_id: sub_id, 
-                                             print_train_set_info = True):
+def create_sort_and_slice_ecfp_featuriser(mols_train, 
+                                          max_radius = 2, 
+                                          pharm_atom_invs = False, 
+                                          bond_invs = True, 
+                                          chirality = False, 
+                                          sub_counts = True, 
+                                          vec_dimension = 1024, 
+                                          break_ties_with = lambda sub_id: sub_id, 
+                                          print_train_set_info = True):
     """
-    Creates a function "ecfp_featuriser" that maps RDKit mol objects to vectorial ECFP fingerprints pooled via a trained Sort & Slice operator (instead of traditional hash-based folding for substructure pooling).
-    See also "Sort & Slice: A Simple and Superior Alternative to Hash-Based Folding for Extended-Connectivity Fingerprints" by Dablander, Hanser, Lambiotte and Morris (2024): https://arxiv.org/abs/2403.17954.
+    Creates a function "ecfp_featuriser" that maps RDKit mol objects to vectorial ECFP fingerprints pooled via a trained Sort & Slice operator (instead of classical hash-based folding).
+    See also "Sort & Slice: A Simple and Superior Alternative to Hash-Based Folding for Extended-Connectivity Fingerprints" by Dablander, Hanser, Lambiotte and Morris (2024): https://arxiv.org/abs/2403.17954
     
     
     INPUTS:
@@ -23,8 +23,8 @@ def construct_sort_and_slice_ecfp_featuriser(mols_train,
     
     - max_radius (int)             ...  The maximal radius up to which to generate the integer ECFP-substructure identifiers. Common choices are 1, 2 or 3 (corresponding to maximal diameters of 2, 4, or 6).
     
-    - pharm_atom_invs (bool)       ...  If False, then the standard initial atomic invariants from RDKit (including ring membership) are used to generate the ECFPs. 
-                                        If True, then instead binary pharmacophoric initial atomic invariants are used to generate what is referred to as FCFPs.
+    - pharm_atom_invs (bool)       ...  If False (= default), then the standard initial atomic invariants from RDKit (including ring membership) are used to generate the ECFPs. 
+                                        If True, then instead binary pharmacophoric initial atomic invariants are used to generate a different type of ECFP also referred to as FCFPs.
     
     - bond_invs (bool)             ...  Whether or not to take into account bond invariants when generating the integer ECFP-substructure identifiers (default = True).
     
@@ -34,14 +34,13 @@ def construct_sort_and_slice_ecfp_featuriser(mols_train,
                                         or integer fingerprints (sub_counts = True) that additionally indicate how many times a substructure is found in the input compound.
     
     - vec_dimension (int)          ...  Length of the vectorial Sort & Slice ECFP. Common choices are 512, 1024, 2048 and 4096. 
-                                        Only the vec_dimension most prevalent ECFP-substructures in mols_train are included in the final vector representation.
+                                        Only the vec_dimension most prevalent ECFP-substructures in the training set mols_train are included in the final vector representation.
     
     - break_ties_with (function)   ...  Function to map the integer ECFP-substructure identifiers to values that are used to break ties when sorting the substructure identifiers according to their prevalence in mols_train. 
                                         The default is the identity map (i.e., lambda sub_id: sub_id) which breaks ties using the (arbitrary) ordering defined by the integer substructure identifier themselves.
                                         If break_ties_with = None, then ties are broken automatically as part of the application of Python's sorted() command to sub_ids_to_prevs_dict.
     
-    - print_train_set_info (bool)  ...  Whether or not to print out the number of compounds and the number of unique integer ECFP-substructure identifiers 
-                                        (i.e., the number of unique circular substructures) with the specified parameters in mols_train.
+    - print_train_set_info (bool)  ...  Whether or not to print out the number of compounds and the number of unique integer ECFP-substructure identifiers with the specified parameters in mols_train.
     
     
     OUTPUT:
@@ -63,14 +62,14 @@ def construct_sort_and_slice_ecfp_featuriser(mols_train,
                                                                break_ties_with = lambda sub_id: sub_id, 
                                                                print_train_set_info = True)
                                                                
-    Note that the ECFP settings (max_radius, pharm_atom_invs, bond_invs, chirality, sub_counts, vec_dimension, break_ties_with) and chemical information from mols_train are all by construction implicitly transferred to "ecfp_featuriser".
-    
+    Note that the ECFP settings (max_radius, pharm_atom_invs, bond_invs, chirality, sub_counts, vec_dimension, break_ties_with) as well as chemical information from mols_train are all by construction implicitly transferred to "ecfp_featuriser".
     Now let mol be an RDKit mol object. Then ecfp_featuriser(mol) is a 1-dimensional numpy array of length vec_dimension representing the vectorial Sort & Slice ECFP fingerprint for mol.
-    
     The function ecfp_featuriser(mol) works by (i) first generating the (multi)set of integer ECFP-substructure identifiers for mol and then (ii) vectorising it via a Sort & Slice operator trained on mols_train (rather than via classical hash-based folding).
     """
     
-    # create substructure enumerator function that maps a mol object to a dictionary whose keys are the integer substructure identifiers in mol and whose values are the associated substructure counts (i.e., how often each substructure appears in mol)
+    
+    
+    # create a function sub_id_enumerator that maps a mol object to a dictionary whose keys are the integer substructure identifiers in mol and whose values are the associated substructure counts (i.e., how often each substructure appears in mol)
     morgan_generator = rdFingerprintGenerator.GetMorganGenerator(radius = max_radius,
                                                                  atomInvariantsGenerator = rdFingerprintGenerator.GetMorganFeatureAtomInvGen() if pharm_atom_invs == True else rdFingerprintGenerator.GetMorganAtomInvGen(includeRingMembership = True),
                                                                  useBondTypes = bond_invs,
@@ -103,13 +102,13 @@ def construct_sort_and_slice_ecfp_featuriser(mols_train,
     # create a function ecfp_featuriser that maps RDKit mol objects to vectorial ECFP fingerprints via a Sort & Slice substructure pooling operator trained on mols_train
     def ecfp_featuriser(mol):
 
-        # create list of integer substructure identifiers contained in input mol object (multiplied by how often they appear in mol if sub_counts = True)
+        # create list of integer substructure identifiers contained in input mol object (multiplied by how often they are structurally contained in mol if sub_counts = True)
         if sub_counts == True:
-            sub_id_list = [sub_id for (sub_id, count) in sub_id_enumerator(mol).items() for sub_id in [sub_id]*count]
+            sub_id_list = [sub_idd for (sub_id, count) in sub_id_enumerator(mol).items() for sub_idd in [sub_id]*count]
         else:
             sub_id_list = list(sub_id_enumerator(mol).keys())
         
-        # create molecule-wide vectorial representation by summing up one-hot encoded substructure idenfitiers
+        # create molecule-wide vectorial representation by summing up one-hot encoded substructure identifiers
         ecfp_vector = np.sum(np.array([sub_id_one_hot_encoder(sub_id) for sub_id in sub_id_list]), axis = 0)
     
         return ecfp_vector
