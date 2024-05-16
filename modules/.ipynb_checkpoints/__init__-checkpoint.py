86f2bffb1e14f63338fbf92818c96e77b6817b4e
Markus Ferdinand Dablander <markus.dablander@gmx.at>
2024-05-16 14:03:41 +0200

    sicherung

--- modules/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from .functions_general import *
-from .functions_data_splitting import *
-from .functions_molecular_processing import *
-from .functions_machine_learning import *
-from .functions_deep_learning_tf_keras import *
-from .functions_deep_learning_pytorch import *
